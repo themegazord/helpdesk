e41e918ab4d6eba2b6327f4d68c5b0ccda2c6334
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-15 21:41:41 -0400

    chore: configurado o metodo index como void e definido a variavel $data para encaminhar os ddos do controller para a view'

--- app/http/controllers/HomeController.php
+++ app/http/controllers/HomeController.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace app\http\controllers;
+use app\infrastructe\mock\HomeNavLinks\NavLinks;
 
 class HomeController {
     public function __construct(
         private readonly NavLinks $navLinks
     ){}
-    public function index() {
-        $navlinks = $this->navLinks->links();
+    public function index(): void {
+        $data = [
+            'navlinks' => $this->navLinks->links()
+        ];
         include 'resources/views/Home/Home.php';
     }
 }
