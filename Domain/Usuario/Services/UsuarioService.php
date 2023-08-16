024d03ad7b2e836b5ed9dc34bb396f1419a5544b
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-16 19:18:40 -0400

    chore: configurado para usar UsuarioRepository

--- Domain/Usuario/Services/UsuarioService.php
+++ Domain/Usuario/Services/UsuarioService.php
@@ -3,9 +3,14 @@
 namespace Domain\Usuario\Services;
 
 use Domain\Usuario\DTO\UsuarioDTO;
+use Domain\Usuario\Interfaces\IUsuario;
 
 class UsuarioService
 {
+    public function __construct(private readonly IUsuario $usuarioRepository)
+    {
+    }
+
     public function cadastro(UsuarioDTO $usuarioDTO) {
         var_dump($usuarioDTO);
     }
