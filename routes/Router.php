e44a21ac9914aa36eaceb5d849ea61ad33c1bdf5
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-15 21:44:08 -0400

    chore: terminado de configurar o container

--- routes/Router.php
+++ routes/Router.php
@@ -2,26 +2,29 @@
 
 namespace routes;
 
+use providers\Container;
+
+require 'app\providers\Container.php';
+
 class Router {
     private $routes = [];
 
-    public function addRoute($url, $controller, $method, array $di) {
-        $this->routes[$url] = ['controller' => $controller, 'method' => $method, 'di' => $di];
+    public function addRoute($url, $controller, $method) {
+        $this->routes[$url] = ['controller' => $controller, 'method' => $method];
     }
 
     /**
      * @throws \ReflectionException
+     * @throws \Exception
      */
     public function route($url) {
+        $container = new Container();
         if (isset($this->routes[$url])) {
             $route = $this->routes[$url];
-            $controllerName = $route['controller'];
+            $controllerName = $container->register($route['controller']);
             $methodName = $route['method'];
-            $di = $route['di'];
 
-//            require_once $controllerName;
-            $controllerPuro = new \ReflectionClass($controllerName);
-            $controller = $controllerPuro->newInstanceArgs($di);
+            $controller = $controllerName;
             $controller->$methodName();
         } else {
             var_dump("404 - Page not found");
