7cc05f508b7e5d5aae00020fd3c1dcd7d7127eed
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-16 15:58:21 -0400

    feat: criado o arquivo UsuarioService para gerir as regras de negocio do domain usuario

---
+++ Domain/Usuario/Services/UsuarioService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Domain\Usuario\Services;
+
+use Domain\Usuario\DTO\UsuarioDTO;
+
+class UsuarioService
+{
+    public function cadastro(UsuarioDTO $usuarioDTO) {
+        var_dump($usuarioDTO);
+    }
+}
