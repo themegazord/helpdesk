cddb98759d9eb75906c3bfed5839240974094ce8
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-16 19:19:51 -0400

    chore: configurado para seguir o contrato do IUsuario

--- infrastructure/Persistence/Repositories/Usuario/UsuarioRepository.php
+++ infrastructure/Persistence/Repositories/Usuario/UsuarioRepository.php
@@ -1,7 +1,23 @@
 <?php
 
-namespace Infrastructure\Persistence\Repositories;
-class UsuarioRepository
-{
+namespace Infrastructure\Persistence\Repositories\Usuario;
 
+use Infrastructure\Database\DatabaseConnection;
+use Domain\Usuario\DTO\UsuarioDTO;
+use Domain\Usuario\Interfaces\IUsuario;
+
+require 'Domain\Usuario\Interfaces\IUsuario.php';
+
+class UsuarioRepository implements IUsuario
+{
+    private $db;
+    public function __construct(private readonly DatabaseConnection $connection)
+    {
+        $this->db = $this->connection->getPdo();
+    }
+
+    public function cadastro(UsuarioDTO $usuarioDTO)
+    {
+
+    }
 }
