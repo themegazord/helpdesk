bb480076dadd43fe5fd55f942c39c0744c47f0a4
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-15 21:46:13 -0400

    feat: criado os componentes de navbar e footer e seus estilos locais

---
+++ resources/views/componentes/footer/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="footer">
+        <p>&copy; 2023 Helpdesk. Todos os direitos reservados.</p>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ resources/views/componentes/navbar/navbar.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/resources/views/css/geral.css">
+    <title>Home - Helpdesk</title>
+</head>
+<body>
+<nav id="navbar">
+    <div id="logo">
+        LOGO
+    </div>
+    <div id="nav-links">
+        <ul class="links">
+            <?php
+            /**
+             * @var array $data
+             */
+            for ($i = 0; $i < count($data['navlinks']); $i += 1) {
+                echo "<li><a class='link' href='{$data['navlinks'][$i]['url']}'>{$data['navlinks'][$i]['label']}</a></li>";
+            }
+            ?>
+        </ul>
+    </div>
+</nav>
