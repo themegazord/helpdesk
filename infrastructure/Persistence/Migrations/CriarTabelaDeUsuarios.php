fce72f7a5bffb0f452c398b3c54083a4702c282b
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-16 21:40:25 -0400

    chore: corrigido script da migration de criacao da tabela de usuarios

--- infrastructure/Persistence/Migrations/CriarTabelaDeUsuarios.php
+++ infrastructure/Persistence/Migrations/CriarTabelaDeUsuarios.php
@@ -3,8 +3,8 @@
 class CriarTabelaDeUsuarios
 {
     public function up() {
-        $sql = "CREATE TABLE IF NOT EXISTS EXISTS usuarios(
-                    usuario_id INT AUTO_INCREMENT PRIMARY_KEY,
+        $sql = "CREATE TABLE IF NOT EXISTS usuarios(
+                    usuario_id INT AUTO_INCREMENT PRIMARY KEY,
                     usuario_nome VARCHAR(155) NOT NULL,
                     usuario_email VARCHAR(155) NOT NULL,
                     usuario_senha VARCHAR(255) NOT NULL)";
