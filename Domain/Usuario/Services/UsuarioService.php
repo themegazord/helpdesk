747bd7ca3aa1c43d99496c0c42e20c201c04a3ad
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-16 22:39:15 -0400

    chore: modificado o metodo para cadastro de usuario e inserido privates functions para validação e criptografia

--- Domain/Usuario/Services/UsuarioService.php
+++ Domain/Usuario/Services/UsuarioService.php
@@ -3,6 +3,7 @@
 namespace Domain\Usuario\Services;
 
 use Domain\Usuario\DTO\UsuarioDTO;
+use Domain\Usuario\Exceptions\UsuarioException;
 use Domain\Usuario\Interfaces\IUsuario;
 
 class UsuarioService
@@ -11,7 +12,17 @@ class UsuarioService
     {
     }
 
-    public function cadastro(UsuarioDTO $usuarioDTO) {
-        var_dump($usuarioDTO);
+    public function cadastro(UsuarioDTO $usuarioDTO): UsuarioException|bool {
+        if (!empty($this->consultaEmailExistente($usuarioDTO->getEmailUsuario()))) return UsuarioException::emailExistente($usuarioDTO->getEmailUsuario());
+        $usuarioDTO->setSenhaUsuario($this->criptografaSenha($usuarioDTO->getSenhaUsuario()));
+        return $this->usuarioRepository->cadastro($usuarioDTO);
+    }
+
+    private function consultaEmailExistente(string $email): string|array {
+        return $this->usuarioRepository->queryUsuarioPorEmail($email);
+    }
+
+    private function criptografaSenha(string $senha): string {
+        return password_hash($senha, PASSWORD_BCRYPT, ['cost' => 12]);
     }
 }
