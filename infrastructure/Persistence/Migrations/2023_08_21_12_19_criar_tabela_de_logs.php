8f374782af096134297be489cd04910f7996b607
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-21 12:53:34 -0400

    feat: criado migration para criar tabela de log

---
+++ infrastructure/Persistence/Migrations/2023_08_21_12_19_criar_tabela_de_logs.php
@@ -0,0 +1,15 @@
+<?php
+
+return new class {
+    public function up(): void {
+        $sql = "create table if not exists log (
+                log_id int auto_increment primary key,
+                timestamp TIMESTAMP not null,
+                nivel varchar(10) not null,
+                mensagem text not null);";
+    }
+
+    public function down(): void {
+        $sql = "drop table if exists log;";
+    }
+};
