e6b909e8f75f3c325bb93cbd02f20f5c00173d02
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-21 11:38:19 -0400

    refactor: refatorado o código do roteador para ser capaz de comportar no maximo um parametro por rota

--- routes/Router.php
+++ routes/Router.php
@@ -3,8 +3,11 @@
 namespace routes;
 
 use app\providers\Container;
+use app\http\controllers\CadastroController;
+use app\http\controllers\HomeController;
 
 require_once 'vendor\autoload.php';
+
 
 class Router {
     private array $routes = [];
@@ -13,19 +16,20 @@ class Router {
         $this->routes[$url] = ['controller' => $controller, 'method' => $method];
     }
 
-    /**
-     * @throws \ReflectionException
-     * @throws \Exception
-     */
     public function route($url): void {
         $container = new Container();
+        $param = null;
+        if (str_contains($url, '?')) {
+            $param = explode('=', explode('?', $url)[1])[1];
+            $url = explode('?', $url)[0];
+        }
         if (isset($this->routes[$url])) {
             $route = $this->routes[$url];
             $controllerName = $container->register($route['controller']);
             $methodName = $route['method'];
 
             $controller = $controllerName;
-            $controller->$methodName();
+            $controller->$methodName($param);
         } else {
             var_dump("404 - Page not found");
         }
