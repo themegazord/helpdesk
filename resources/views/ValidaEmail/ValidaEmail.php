fa290df4c3bd8c91233a6fab3867a048b3936c5c
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-21 20:29:44 -0400

    chore: alterado para receber código em vez de email

--- resources/views/ValidaEmail/ValidaEmail.php
+++ resources/views/ValidaEmail/ValidaEmail.php
@@ -3,16 +3,16 @@
         <div class="container-form-valida-email">
             <div class="header-valida-email">
                 <h1 class="titulo-valida-email">Validação de Email</h1>
-                <p class="sub-titulo-valida-email">Insira seu email abaixo para continuar o processo de validação.</p>
+                <p class="sub-titulo-valida-email">Insira o código que foi encaminhado para o seu email.</p>
             </div>
             <form action="/cadastro/validaemail/process" method="post" class="form-valida-email">
-                <label for="email" class="label-email-valida-email">Email:</label>
-                <input type="email" id="email" name="email" class="input-email-valida-email" required>
+                <label for="cod" class="label-cod-valida-email">Código:</label>
+                <input type="cod" id="cod" name="cod" class="input-cod-valida-email" required>
                 <br>
                 <button type="submit" class="botao-valida-email">Continuar</button>
             </form>
             <div class="footer-valida-email">
-                <p class="info-valida-email">Nós iremos enviar um link de validação para o endereço de email fornecido.
+                <p class="info-valida-email">Nós iremos enviar um código de validação para o endereço de email fornecido.
                     Certifique-se de verificar sua caixa de entrada e pasta de spam.</p>
             </div>
         </div>
