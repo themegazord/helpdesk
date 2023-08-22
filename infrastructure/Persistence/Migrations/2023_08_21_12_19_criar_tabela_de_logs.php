f8f8cdc464cdc5db5750e312aa776a1ecff6435c
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-21 20:29:09 -0400

    chore: alterado o tipo e o campo da coluna timestamps para data_log e date

--- infrastructure/Persistence/Migrations/2023_08_21_12_19_criar_tabela_de_logs.php
+++ infrastructure/Persistence/Migrations/2023_08_21_12_19_criar_tabela_de_logs.php
@@ -4,7 +4,7 @@ return new class {
     public function up(): void {
         $sql = "create table if not exists log (
                 log_id int auto_increment primary key,
-                timestamp TIMESTAMP not null,
+                data_log date not null,
                 nivel varchar(10) not null,
                 mensagem text not null);";
     }
