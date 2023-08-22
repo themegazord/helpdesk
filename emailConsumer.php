aacd605c685e5ca877fd24e8f1de2b0246fc391a
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-22 12:42:44 -0400

    chore: alterado poucas coisas mais semanticas de codigo

--- emailConsumer.php
+++ emailConsumer.php
@@ -6,17 +6,17 @@ use Domain\EnvioEmail\Services\EnvioEmailService;
 require_once __DIR__ . '/vendor/autoload.php';
 
 $envioEmail = new EnvioEmailService();
+
 $conexao = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $canal = $conexao->channel();
 
 $canal->queue_declare('emailCodigoVerificacao', false, true, false, false);
 
 $callback = function ($msg) use (&$envioEmail) {
-    echo "Mensagem recebida...\n";
     $envioEmail->enviaCodigoVerificacao($msg->body);
-    echo $msg->body;
-    echo "\nResposta enviada...\n";
+    echo "Mensagem recebida...\n";
     $msg->ack();
+    echo "\nResposta enviada...\n";
 };
 
 $canal->basic_qos(null, 1, null);
