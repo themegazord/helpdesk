9e6bf98ebd264f078920d624e6902e24ae40c334
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-22 12:40:58 -0400

    chore: configurado para usar PHPMailler

--- Domain/EnvioEmail/Services/EnvioEmailService.php
+++ Domain/EnvioEmail/Services/EnvioEmailService.php
@@ -3,21 +3,35 @@
 namespace Domain\EnvioEmail\Services;
 
 use Domain\EnvioEmail\DTO\EnvioEmailDTO;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
 
 require 'vendor\autoload.php';
 
 class EnvioEmailService
 {
+    /**
+     * @throws Exception
+     */
     public function enviaCodigoVerificacao(string $resposta): void {
-        $email = explode('=', explode(';', $resposta)[0])[1];
-        $codigo = explode('=', explode(';', $resposta[1])[1]);
+
+        $dotenv = Dotenv::createUnsafeImmutable(__DIR__ . '/../../../');
+        $dotenv->load();
 
         $envioEmail = new EnvioEmailDTO();
-        $envioEmail->setDestinatario($email);
-        $envioEmail->setCabecalho([
-            'MIME-Version: 1.0',
-            'Content-type: text/html; charset=iso-8859-1'
-        ]);
+        $phpMailler = new PHPMailer(true);
+
+        $respostaArray = explode(';', $resposta);
+        $email = explode('=', $respostaArray[0])[1];
+        $codigo = explode('=', $respostaArray[1])[1];
+
+        $header = "MIME-Version: 1.1\r\n";
+        $header .= "Content-type: text/html; charset=iso-8859-1\r\n";
+        $header .= "From: lilith.sacrifate@gmail.com\r\n";
+
+        $envioEmail->setCabecalho($header);
         $envioEmail->setTitulo('Seu código de verificação está pronto!');
         $envioEmail->setCorpo("
             <html>
@@ -30,7 +44,30 @@ class EnvioEmailService
                 </body>
             </html>
         ");
+        $envioEmail->setDestinatario($email);
 
-        mail($envioEmail->getDestinatario(), $envioEmail->getTitulo(), $envioEmail->getCorpo(), $envioEmail->getCabecalho());
+        try {
+            $phpMailler->SMTPDebug = SMTP::DEBUG_SERVER;
+            $phpMailler->isSMTP();
+            $phpMailler->Host       = getenv('PHP_MAILLER_HOST');
+            $phpMailler->SMTPAuth   = true;
+            $phpMailler->Username   = getenv('PHP_MAILLER_USUARIO');
+            $phpMailler->Password   = getenv('PHP_MAILLER_PASSWORD');
+            $phpMailler->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $phpMailler->Port       = getenv('PHP_MAILLER_PORT');
+            $phpMailler->CharSet    = 'utf-8';
+
+            $phpMailler->setFrom(getenv('PHP_MAILLER_USUARIO'), 'Helpdesk');
+            $phpMailler->addAddress($email);
+
+            $phpMailler->isHTML(true);
+            $phpMailler->Subject = $envioEmail->getTitulo();
+            $phpMailler->Body    = $envioEmail->getCorpo();
+
+            $phpMailler->send();
+            echo "Email enviado com sucesso \n";
+        }catch(\Exception $e) {
+            echo "Mensagem não pode ser enviada. Mailler error: {$phpMailler->ErrorInfo} \n";
+        }
     }
 }
