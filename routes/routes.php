3ca55e4baa4fae3adb8eb25a08a2f210caeb6bd6
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-22 16:10:47 -0400

    chore: adicionado rota para processamento de dados da validação de email

--- routes/routes.php
+++ routes/routes.php
@@ -9,6 +9,7 @@ $router->addRoute('/', 'HomeController', 'index');
 $router->addRoute('/cadastro', 'CadastroController', 'index');
 $router->addRoute('/cadastro-process', 'CadastroController', 'processaDadosCadastro');
 $router->addRoute('/cadastro/validaemail', 'CadastroController', 'validaEmail');
+$router->addRoute('/cadastro/validaemail-process', 'CadastroController', 'procassaDadosValidaEmail');
 
 // Get the requested URL from the user
 $requestUrl = $_SERVER['REQUEST_URI'];
