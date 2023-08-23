499110880df455f7e3037b75ae07d9930c63e85a
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-23 13:25:44 -0400

    chore: iniciado a sessão no __construct

--- app/http/controllers/HomeController.php
+++ app/http/controllers/HomeController.php
@@ -3,7 +3,9 @@
 namespace app\http\controllers;
 
 class HomeController {
-    public function __construct(){}
+    public function __construct(){
+        session_start();
+    }
     public function index(): void {
         include 'resources/views/Home/HomeView.php';
     }
