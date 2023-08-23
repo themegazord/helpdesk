d86d40a1a2f06ff75f2b2710162dd60185309596
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-23 13:26:31 -0400

    chore: configurado session no construct e criado o metodo para tratar os dados do login

--- app/http/controllers/LoginController.php
+++ app/http/controllers/LoginController.php
@@ -1,10 +1,43 @@
 <?php
 
+
 namespace app\http\controllers;
+
+
+use app\FormRequest\Autenticacao\LoginRequest;
+use Domain\Usuario\DTO\UsuarioDTO;
+use Domain\Usuario\Exceptions\UsuarioException;
+
+require 'routes\routeHelpers.php';
+
+require_once 'vendor\autoload.php';
 
 class LoginController
 {
+    public function __construct(
+        public readonly LoginRequest $loginRequest
+    )
+    {
+        session_start();
+    }
+
     public function index(): void {
         include('resources\views\Login\LoginView.php');
+    }
+
+    public function processaDadosLogin(): void {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $usuario = new UsuarioDTO();
+            $usuario->setEmailUsuario($_POST['email-login']);
+            $usuario->setSenhaUsuario($_POST['senha-login']);
+            try {
+                $respostaLogin = $this->loginRequest->dispatch($usuario);
+                $_SESSION['email'] = $respostaLogin['email'];
+                $_SESSION['nome'] = $respostaLogin['nome'];
+                redirect('/');
+            } catch (UsuarioException $ue) {
+                redirect('/login?erro=' . urlencode($ue->getMessage()));
+            }
+        }
     }
 }
