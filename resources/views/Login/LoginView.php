72773d94dac885a89ecaa8bf4a4a23b66cd00d50
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-22 21:42:00 -0400

    feat: criado view do login

---
+++ resources/views/Login/LoginView.php
@@ -0,0 +1,6 @@
+<?php include 'resources\views\componentes\navbar\navbar.php'?>
+<?php include 'resources\views\componentes\notificacao\notificacao.php'?>
+
+<h1>Login</h1>
+
+<?php include 'resources\views\componentes\footer\footer.php'?>
