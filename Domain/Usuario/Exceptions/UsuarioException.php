7935e7d09e55850e8b4b594a5a9ad41f1be1e3d1
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-23 13:21:15 -0400

    chore: adicionado exceções utilizadas no login

--- Domain/Usuario/Exceptions/UsuarioException.php
+++ Domain/Usuario/Exceptions/UsuarioException.php
@@ -11,6 +11,10 @@ class UsuarioException extends \Exception
         throw new self('O email não pode ser vazio');
     }
 
+    public static function senhaEObrigatorio(): self {
+        throw new self('A senha não pode ser vazio');
+    }
+
     /**
      * @throws UsuarioException
      */
@@ -18,10 +22,30 @@ class UsuarioException extends \Exception
         throw new self(message: 'O email ' . $email . ' é inválido');
     }
 
+    /**
+     * @throws UsuarioException
+     */
+    public static function senhaInvalida(): self {
+        throw new self('Senha inválida');
+    }
+
+    /**
+     * @throws UsuarioException
+     */
     public static function emailExistente(string $email): self {
         throw new self('O email ' . $email . ' já existe, por favor, entre no sistema.');
     }
 
+    /**
+     * @throws UsuarioException
+     */
+    public static function emailInexistente(string $email): self {
+        throw new self('O email ' . $email . ' não existe, por favor, cadastre-se.');
+    }
+
+    /**
+     * @throws UsuarioException
+     */
     public static function codigoDifereDoEmail(): self {
         throw new self('O código que você informou não é o mesmo que você recebeu no email, por favor, verifique.');
     }
