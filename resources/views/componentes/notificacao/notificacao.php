18914b2c5280e03626e97d5a437f8035ed64b0aa
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-22 22:34:12 -0400

    chore: alterado a classe da notificacao

--- resources/views/componentes/notificacao/notificacao.php
+++ resources/views/componentes/notificacao/notificacao.php
@@ -1,6 +1,6 @@
 <?php if($_SERVER['REQUEST_METHOD'] == 'GET'): ?>
     <?php if(isset($_GET['erro'])): ?>
-        <div id='notificacao-cadastro-usuario' class='notificacao-cadastro-usuario'>
+        <div id='notificacao-cadastro-usuario' class='notificacao'>
             <p><?= urldecode($_GET['erro']); ?></p>
         </div>
     <?php endif; ?>
