20bc8c655672fb459f89052fac443fc66ce4fabd
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-25 11:53:14 -0400

    feat: criado componente header

---
+++ resources/views/componentes/header/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/resources/views/css/geral.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <title>Home - Helpdesk</title>
+    </head>
+    <body>
