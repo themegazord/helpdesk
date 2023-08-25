424d1613f467ab55febc86fdea2f6dd671b090c7
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-25 15:55:39 -0400

    chore: inserido rota para dashboard'

--- routes/routes.php
+++ routes/routes.php
@@ -12,6 +12,7 @@ $router->addRoute('/cadastro/validaemail', 'CadastroController', 'validaEmail');
 $router->addRoute('/cadastro/validaemail-process', 'CadastroController', 'procassaDadosValidaEmail');
 $router->addRoute('/login', 'LoginController', 'index');
 $router->addRoute('/login-process', 'LoginController', 'processaDadosLogin');
+$router->addRoute('/dashboard', 'DashboardController', 'index');
 
 // Get the requested URL from the user
 $requestUrl = $_SERVER['REQUEST_URI'];
