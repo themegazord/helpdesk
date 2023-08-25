471019640bda66b8f685fadd98b8df0a8955b258
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-25 15:59:02 -0400

    feat: view do dashboard

---
+++ resources/views/Dashboard/DashboardView.php
@@ -0,0 +1,9 @@
+<?php include 'resources\componentes\header\header.php';?>
+<main class="d-flex flex-no-wrap">
+    <?php include 'resources\componentes\sidebar\sidebar.php'; ?>
+    <div class="container-dashboard">
+        <h1>Teste</h1>
+    </div>
+</main>
+
+<?php include 'resources\componentes\footer\footer.php';?>
