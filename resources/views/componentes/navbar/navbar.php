e20f873af40cf4a3fca60402d1bc1c6c22448d33
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-25 11:53:44 -0400

    chore: retirado o header do html principal para criar a parte e incluido o mesmo

--- resources/views/componentes/navbar/navbar.php
+++ resources/views/componentes/navbar/navbar.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/resources/views/css/geral.css">
-    <title>Home - Helpdesk</title>
-</head>
-<body>
+<?php include 'resources/views/componentes/header/header.php'?>
 <nav id="navbar">
     <div id="logo">
         LOGO
