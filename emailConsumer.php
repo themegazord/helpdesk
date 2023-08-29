a42d20bc1b578b2b8d2b589ab2b9666d717cf5d9
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-29 14:33:45 -0400

    chore: removido comentario de códigos passados

--- emailConsumer.php
+++ emailConsumer.php
@@ -4,33 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use Domain\EnvioEmail\Services\EnvioEmailService;
 
-
-
-//
-//$conexao = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
-//$canal = $conexao->channel();
-//
-//$canal->queue_declare('emailCodigoVerificacao', false, true, false, false);
-//
-//$callback = function ($msg) use (&$envioEmail) {
-//    $envioEmail->enviaCodigoVerificacao($msg->body);
-//    echo "Mensagem recebida...\n";
-//    $msg->ack();
-//    echo "\nResposta enviada...\n";
-//};
-//
-//$canal->basic_qos(null, 1, null);
-//$canal->basic_consume('emailCodigoVerificacao', '', false, false, false, false, $callback);
-//
-//echo "Esperando mensagens novas... \n";
-//
-//while($canal->is_open()) {
-//    $canal->wait();
-//}
-//
-//$canal->close();
-//$conexao->close();
-
 $envioEmail = new EnvioEmailService();
 
 $conexao = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
